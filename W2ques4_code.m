f18c49d344bc86c41d84f8b822484c58adc88477
snehadubey9066 <59720911+snehadubey9066@users.noreply.github.com>
2020-12-11 11:44:37 +0530

    Updating code for checking time reposne

--- W2ques4_code.m
+++ W2ques4_code.m
@@ -3,24 +3,33 @@
 % % Overshoot less than 16%
 % % No steady-state error, even in the presence of a step disturbance input
 
-t = 0:0.001:0.2;
 J = 3.2284E-6;
 b = 3.5077E-6;
 K = 0.0274;
-Ke = 0.0274;
+Ke = 0.0274854;
 R = 4;
 L = 2.75E-6;
 
 s = tf('s');
 P_motor = K/(s*((J*s+b)*(L*s+R)+K^2));
-
+ 
 Kp = 1;
 Ki = 1;
 Kd = 1;
+N = 1;
+C = Kp+Ki/s+Kd*N/(1+N/s);
 
-C = pid(Kp,Ki,Kd);
 sys_cl = feedback(C*P_motor,1);
-Risetime_PID = stepinfo(sys_cl).RiseTime
-OverShoot_PID = stepinfo(sys_cl).Overshoot
+t = 0:0.001:0.2;
+step(sys_cl)
+grid
 
-step(sys_cl,t)
+% % dist_cl = feedback(P_motor,C);
+% % t = 0:0.001:0.2;
+% % step(dist_cl)
+
+rs = stepinfo(sys_cl).RiseTime
+Ov = stepinfo(sys_cl).Overshoot
+
+sim("W2ques4_system.slx")
+open_system("W2ques4_system/Scope")
